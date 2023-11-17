c9d5e9605f97c7125b47e1228107f154589d613c
Felipe Kurjata <fkurjata@gmail.com>
2023-11-17 19:11:10 -0300

    seeders

---
+++ config/Seeds/UserSeed.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+use faker\Factory;
+
+/**
+ * User seed.
+ */
+class UserSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $data = [];
+
+        $faker = Factory::create();
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
