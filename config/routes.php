9018d6e41797d18c90a586fd875e306a929767d2
Felipe Kurjata <fkurjata@gmail.com>
2023-11-19 21:31:33 -0300

    construindo as rotas e controllers

--- config/routes.php
+++ config/routes.php
@@ -4,5 +4,6 @@ use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
 return static function (RouteBuilder $routes) {
-    $routes->connect('/', ['controller' => 'Home', 'action' => 'index']);
+    $routes->connect('/', ['controller' => 'Home', 'action' => 'index'],['_name' => 'home.index'])->setMethods(['get']);
+    $routes->connect('/about', ['controller' => 'About', 'action' => 'index'], ['_name' => 'about.index'])->setMethods(['get', 'post']);
 };
